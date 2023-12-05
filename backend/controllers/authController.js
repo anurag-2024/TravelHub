c6a54656e738af9303a1d8feddf559fe0911ea8d
anurag-2420 <anuragpatel20june@gmail.com>
2023-12-05 12:42:46 +0530

    Bug fixed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,18 +27,18 @@ export const register = async (req, res) => {
 }
 export const login = async (req, res) => {
     try {
-        const { email, password } = req.body; 
+        const { email, password } = req.body;
         await User.findOne({ email })
             .then(async (user) => {
-                if(user.emailVerified==="false"){
-                    return res.status(401).json({message:"Please verify your email first"});
+                if (user.emailVerified === "false") {
+                    return res.status(401).json({ message: "Please verify your email first" });
                 }
                 await bcrypt.compare(password, user.password)
                     .then((passwordCheck) => {
                         if (!passwordCheck) {
-                            return res.status(400).json({message:"Invalid Credentials"});
+                            return res.status(400).json({ message: "Invalid Credentials" });
                         }
-                        const {password,role,...rest}=user._doc;
+                        const { password, role, ...rest } = user._doc;
                         const token = jwt.sign({
                             userId: user._id,
                             email: user.email,
@@ -51,115 +51,121 @@ export const login = async (req, res) => {
                         }).json({
                             success: true,
                             message: "User logged in successfully",
-                            data: {...rest},
+                            data: { ...rest },
                             token,
                             role
                         })
                     })
-                    .catch((err)=>{
+                    .catch((err) => {
                         return res.status(404).send(err.message);
                     })
             })
             .catch((err) => {
-                return res.status(404).json({message:"User not found"});
+                return res.status(404).json({ message: "User not found" });
             })
     }
     catch (err) {
         res.status(500).json({ message: err.message })
     }
 }
-export const verifyEmail = async (req,res,next) => {
-   try{
-    const decodedEmail = decodeURIComponent(req.query.email);
-      const user=await User.findOne({email:decodedEmail});
-        if(!user){
+export const verifyEmail = async (req, res, next) => {
+    try {
+        const decodedEmail = decodeURIComponent(req.query.email);
+        const user = await User.findOne({ email: decodedEmail });
+        if (!user) {
             return res.status(404).send({
-                message:"User not found"
+                message: "User not found"
             })
         }
         next();
-   }
-   catch(err){
-    res.status(500).json({ message: err.message })
-   }
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 }
-export function localVariables(req,res,next){
-    req.app.locals={
-      OTP:null,
-      resetSession:false
+export function localVariables(req, res, next) {
+    req.app.locals = {
+        OTP: null,
+        resetSession: false
     }
     next();
 }
 export async function generateOTP(req, res) {
-    req.app.locals.OTP= otpGenerator.generate(6, {specialChars: false, lowerCaseAlphabets: false,upperCaseAlphabets:false});
-    res.status(201).send({code:req.app.locals.OTP});
+    req.app.locals.OTP = otpGenerator.generate(6, { specialChars: false, lowerCaseAlphabets: false, upperCaseAlphabets: false });
+    res.status(201).send({ code: req.app.locals.OTP });
 }
 
 export async function verifyOTP(req, res) {
-    const {code}=req.query;
-    if(parseInt(code)===parseInt(req.app.locals.OTP)){
-        req.app.locals.OTP=null;
-        req.app.locals.resetSession=true;
-        return res.status(200).send({message:"OTP Verified"});
+    const { code } = req.query;
+    if (parseInt(code) === parseInt(req.app.locals.OTP)) {
+        req.app.locals.OTP = null;
+        req.app.locals.resetSession = true;
+        return res.status(200).send({ message: "OTP Verified" });
     }
-    return res.status(400).send({error:"Invalid OTP"});
+    return res.status(400).send({ error: "Invalid OTP" });
 }
 
 export async function createResetSession(req, res) {
-    if(req.app.locals.resetSession){
-        return res.status(201).send({flag:req.app.locals.resetSession});
+    if (req.app.locals.resetSession) {
+        return res.status(201).send({ flag: req.app.locals.resetSession });
     }
-    return res.status(400).send({error:"Session expired"});
+    return res.status(400).send({ error: "Session expired" });
 }
 
 export async function resetPassword(req, res) {
-    try{
-        if(!req.app.locals.resetSession) return res.status(400).send({error:"Session expired"});
-        const email=req.body.email;
-        const password=req.body.password;
-        console.log(email,password);
-        try{
+    try {
+        if (!req.app.locals.resetSession) return res.status(400).send({ error: "Session expired" });
+        const email = req.body.email;
+        const password = req.body.password;
+        console.log(email, password);
+        try {
             const decodedEmail = decodeURIComponent(email);
-             User.findOne({email:decodedEmail})
-             .then(user=>{
-                 bcrypt.hash(password,10)
-                 .then(hashedPassword=>{
-                    User.updateOne({email:user.email},{password:hashedPassword})
-                    .then(user=>{
-                        req.app.locals.resetSession=false;
-                        return res.status(200).send(user);
-                    })
-                    .catch(err=>{
-                        return res.status(400).send(err.message);
-                    })
-                 })
-                 .catch(err=>{
-                    return res.status(500).send(err.message);
-                 })
-             })
+            User.findOne({ email: decodedEmail })
+                .then(user => {
+                    bcrypt.hash(password, 10)
+                        .then(hashedPassword => {
+                            User.updateOne({ email: user.email }, { password: hashedPassword })
+                                .then(user => {
+                                    req.app.locals.resetSession = false;
+                                    return res.status(200).send(user);
+                                })
+                                .catch(err => {
+                                    return res.status(400).send(err.message);
+                                })
+                        })
+                        .catch(err => {
+                            return res.status(500).send(err.message);
+                        })
+                })
         }
-        catch(err){
+        catch (err) {
             return res.status(404).send(err.message);
         }
     }
-    catch(err){
+    catch (err) {
         return res.status(401).send(err.message);
     }
 }
 
 export async function confirmEmail(req, res) {
-    try{
-        const email=req.query.email;
+    try {
+        const email = req.query.email;
         const decodedEmail = decodeURIComponent(email);
-        User.updateOne({email:decodedEmail},{emailVerified:"true"})
-        .then(user=>{
-            return res.status(200).send({user,message:"Email verified successfully"});
-        })
-        .catch(err=>{
-            return res.status(400).send(err.message);
-        })
+        User.findOne({ email: decodedEmail })
+            .then(user => {
+                if (user.emailVerified === "true") {
+                    return res.status(200).send({ user, message: "Email already verified" });
+                }
+                User.updateOne({ email:user.email }, { emailVerified: "true" })
+                    .then(user => {
+                        return res.status(200).send({ user, message: "Email verified successfully" });
+                    })
+                    .catch(err => {
+                        return res.status(400).send(err.message);
+                    })
+            })
     }
-    catch(err){
+    catch (err) {
         return res.status(401).send(err.message);
     }
 }
